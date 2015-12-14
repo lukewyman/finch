00e08245bbd42fd4af639c1becbaaf8982f51fca
Vladimir Kostyukov <vkostyukov@twitter.com>
2015-12-14 03:23:14 -0800

    Unify RequestReaders and Endpoints

--- oauth2/src/main/scala/io/finch/oauth2/package.scala
+++ oauth2/src/main/scala/io/finch/oauth2/package.scala
@@ -1,24 +1,29 @@
 package io.finch
 
-import com.twitter.finagle.http.Request
+import cats.Eval
 import com.twitter.finagle.OAuth2
-import com.twitter.finagle.oauth2.{AuthInfo, DataHandler, GrantHandlerResult}
-import com.twitter.util.Future
-import io.finch.items._
+import com.twitter.finagle.http.Status
+import com.twitter.finagle.oauth2.{AuthInfo, DataHandler, GrantHandlerResult, OAuthError}
 
 package object oauth2 {
 
-  private[this] object OAuth2 extends OAuth2
+  private[this] object oa2 extends OAuth2
+  private[this] val handleOAuthError: PartialFunction[Throwable, Output[Nothing]] = {
+    case e: OAuthError =>
+      val bearer = Seq("error=\"" + e.errorType + "\"") ++
+        (if (!e.description.isEmpty) Seq("error_description=\"" + e.description + "\"") else Nil)
 
-  def authorize[U](dataHandler: DataHandler[U]): RequestReader[AuthInfo[U]] =
-    new RequestReader[AuthInfo[U]] {
-      val item: RequestItem = MultipleItems
-      def apply(req: Request): Future[AuthInfo[U]] = OAuth2.authorize(req, dataHandler)
-    }
+      Output.failure(e, Status(e.statusCode))
+        .withHeader("WWW-Authenticate" -> s"Bearer ${bearer.mkString(", ")}")
+  }
 
-  def issueAccessToken[U](dataHandler: DataHandler[U]): RequestReader[GrantHandlerResult] =
-    new RequestReader[GrantHandlerResult] {
-      val item: RequestItem = MultipleItems
-      def apply(req: Request): Future[GrantHandlerResult] = OAuth2.issueAccessToken(req, dataHandler)
-    }
+  def authorize[U](dataHandler: DataHandler[U]): Endpoint[AuthInfo[U]] =
+    Endpoint.embed(items.MultipleItems)(i =>
+      Some((i, Eval.now(oa2.authorize(i.request, dataHandler).map(ai => Output.payload(ai)))))
+    ).handle(handleOAuthError)
+
+  def issueAccessToken[U](dataHandler: DataHandler[U]): Endpoint[GrantHandlerResult] =
+    Endpoint.embed(items.MultipleItems)(i =>
+      Some((i, Eval.now(oa2.issueAccessToken(i.request, dataHandler).map(t => Output.payload(t)))))
+    ).handle(handleOAuthError)
 }
